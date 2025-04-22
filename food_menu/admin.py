557cb9791db1388b6e5a9936a9be3861a4088243
Ayan Chowdhury <50476025+Ayan-Chowdhury@users.noreply.github.com>
2025-04-22 15:29:06 +0600

    Video 21 How to retrive data.

--- food_menu/admin.py
+++ food_menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['item_name','item_desc','item_price']
+
+
+admin.site.register(Item,ItemAdmin)
