99b22370dd33d8a395f58d4876da2864aca7a4b1
Ayan Chowdhury <ayan.cchowdhury@gmail.com>
2025-05-03 20:12:59 +0600

    Auto stash before merge of "master" and "origin/master"

--- food_menu/admin.py
+++ food_menu/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from .models import Item
 
 # Register your models here.
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ['item_name','item_desc','item_price']
-
-
-admin.site.register(Item,ItemAdmin)
+admin.site.register(Item)
