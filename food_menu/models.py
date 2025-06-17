261e124bae37fd19cb925a165fe2df2f4c6f8322
Ayan Chowdhury <50476025+Ayan-Chowdhury@users.noreply.github.com>
2025-06-17 17:42:59 +0600

    Navbar and Base Html

--- food_menu/models.py
+++ food_menu/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200 )
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_imgae=models.CharField(max_length=500, default="https://www.thefuzzyduck.co.uk/wp-content/uploads/2024/05/image-coming-soon-placeholder-01-660x660.png")
     
     def __str__(self):
         return str(self.item_name)
